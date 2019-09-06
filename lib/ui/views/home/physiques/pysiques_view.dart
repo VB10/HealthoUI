250f65f9bd2d02a502ab05f55788213a25709a11
Veli <Bacik>
2019-09-07 00:15:56 +0300

    healtho ui 5 complete pysiques add number compenet add

--- lib/ui/views/home/physiques/pysiques_view.dart
+++ lib/ui/views/home/physiques/pysiques_view.dart
@@ -3,6 +3,7 @@ import 'package:healthoui/core/model/physique.dart';
 import 'package:healthoui/ui/shared/text_style.dart';
 import 'package:healthoui/ui/shared/ui_helper.dart';
 import 'package:healthoui/ui/shared/widget/rounded_button_widget.dart';
+import 'package:numberpicker/numberpicker.dart';
 
 import 'radio_view.dart';
 
@@ -26,15 +27,10 @@ class _PysiquesViewState extends State<PysiquesView> {
       body: Column(
         mainAxisSize: MainAxisSize.min,
         children: <Widget>[
-          Spacer(
-            flex: 2,
-          ),
-          Text(
-            "Enter Your Physiques",
-            style: profileHeaderCardTitle,
-          ),
+          Spacer(flex: 2),
+          Text("Enter Your Physiques", style: profileHeaderCardTitle),
           Expanded(
-            flex: 5,
+            flex: 6,
             child: Container(
               alignment: Alignment.center,
               margin: EdgeInsets.symmetric(horizontal: 20),
@@ -48,7 +44,7 @@ class _PysiquesViewState extends State<PysiquesView> {
                   _roundedButton(
                       text: "Height",
                       rightText: _physique.height,
-                      onPress: () {}),
+                      onPress: showHeightDialog),
                   _roundedButton(
                       text: "Weight",
                       rightText: _physique.weight,
@@ -57,13 +53,47 @@ class _PysiquesViewState extends State<PysiquesView> {
                       text: "Level",
                       rightText: _physique.level,
                       onPress: showLevelDialog),
+                  UIHelper.verticalSpaceLarge,
+                  RoundedButtonWidget(
+                    color: UIHelper.EXERCISES_CARD_BOTTOM,
+                    onPress: () {},
+                    child: ListTile(
+                      title: Text(
+                        "Confirm Detail",
+                        textAlign: TextAlign.center,
+                        style: profileHeaderCardTitle,
+                      ),
+                    ),
+                  ),
                 ],
               ),
             ),
-          )
+          ),
         ],
       ),
     );
+  }
+
+  double _currentDoubleValue = 3.0;
+  Future showHeightDialog() async {
+    var value = await showDialog<double>(
+      context: context,
+      builder: (BuildContext context) {
+        return NumberPickerDialog.decimal(
+          maxText: "kg",
+          minText: "wh",
+          minValue: 1,
+          maxValue: 5,
+          decimalPlaces: 2,
+          initialDoubleValue: _currentDoubleValue,
+          title: Text("Pick a height number"),
+        );
+      },
+    );
+    if (value != null) {
+      _physique.height = _currentDoubleValue.toString();
+      setState(() {});
+    }
   }
 
   void showWeightDialog() {
@@ -104,7 +134,10 @@ class _PysiquesViewState extends State<PysiquesView> {
                     ),
                   ),
                   Center(
-                    child: Text("kg",style: profileHeaderCardTitle,),
+                    child: Text(
+                      "kg",
+                      style: profileHeaderCardTitle,
+                    ),
                   ),
                   Spacer(),
                 ],
@@ -180,9 +213,16 @@ class _PysiquesViewState extends State<PysiquesView> {
           color: Colors.grey[100],
           onPress: onPress,
           child: ListTile(
-            title: Text(text),
-            trailing: Text(rightText),
-          ),
+              title: rightText.isEmpty
+                  ? Text(text, textAlign: TextAlign.center)
+                  : null,
+              leading: rightText.isEmpty ? null : Text(text),
+              trailing: rightText.isEmpty
+                  ? null
+                  : Text(rightText,
+                      textAlign: rightText.isEmpty
+                          ? TextAlign.center
+                          : TextAlign.start)),
         ),
       ),
     );
